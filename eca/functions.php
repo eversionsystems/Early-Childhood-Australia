91f9107060ffe8568217f2d4cf67a08f9fec2339
Andrew Schultz <contact@eversionsystems.com>
2016-07-21 23:38:04 +0930

    Capitalise text
    Changed the 'item' and 'items' text in the menu cart button to capitals.

--- eca/functions.php
+++ eca/functions.php
@@ -218,7 +218,7 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
 	ob_start();
 
 	?>
-	<a class="cart-contents" id="button-cart" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a> 
+	<a class="cart-contents" id="button-cart" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '%d ITEM', '%d ITEMS', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a> 
 	<?php
 	
 	$fragments['a.cart-contents'] = ob_get_clean();
