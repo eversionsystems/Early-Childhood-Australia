bcce5e7ac07662da949c7991dafe4a7448f12d55
Andrew Schultz <contact@eversionsystems.com>
2016-08-22 14:22:35 +0930

    Packling List Slips Order SKU
    Removed the category headings for Packing Slips and ordered the list by
    SKU.

--- eversion-systems/woocommerce-functions.php
+++ eversion-systems/woocommerce-functions.php
@@ -19,7 +19,7 @@ function eca_manage_woocommerce_styles(){
 	//$slug = get_post( $post )->post_name;
 
 	//if($slug == 'my-account'){
-	wp_enqueue_style('eca-woocommerce-custom', constant( 'EVERSION_PLUGIN_URL' ).'/woocommerce-custom.css', '', '1.0');
+	wp_enqueue_style('eca-woocommerce-custom', constant( 'EVERSION_PLUGIN_URL' ).'/woocommerce-custom.css', '', '1.01');
 	//}
 }
 
@@ -1406,5 +1406,51 @@ function es_filter_products_query( &$query ) {
 			$query->query_vars['meta_query'] = $meta_query_array;
 	}
 }
+
+/**
+ * Change the order items sort order in documents in v3.0.2+
+ *
+ * @param string $sort_order_items_key The column key (such as 'sku', 'price', 'weight', etc.) to sort order items by
+ * @param int $order_id The WC_Order id
+ * @param string $document_type The type of document being viewed
+ * @return string The filtered sort column key
+ */
+function sv_wc_pip_document_sort_order_items_key( $sort_by, $order_id, $type ) {
+	// sort order items in all document types by SKU.
+	$sort_by = 'sku';
+
+	return $sort_by;
+}
+
+add_filter( 'wc_pip_document_sort_order_items_key', 'sv_wc_pip_document_sort_order_items_key', 10, 3 );
+
+/**
+ * Removes packing list / pick list sorting by category and
+ *   outputs line items in alphabetical order
+ */
+add_filter( 'wc_pip_packing_list_group_items_by_category', '__return_false' );
+
+/**
+ * Example: Remove product grouping if an order is not yet paid. (WC 2.5+)
+ * Only removes grouping for pick lists
+ * Requires PIP 3.1.1+
+ *
+ * @param bool $group_items true if items should be grouped by category
+ * @param int $order_id the ID for the document's order
+ * @param string $document_type the type for the current document
+ * @return bool
+ */
+function sv_wc_pip_packing_list_grouping( $group_items, $order_id, $document_type ) {
+	// bail unless we're looking at a pick list
+	if ( 'pick-list' !== $document_type ) {
+		return $group_items;
+	}
+	$order = wc_get_order( $order_id );
+	if ( ! $order->is_paid() ) {
+		return false;
+	}
+	return $group_items;
+}
+add_filter( 'wc_pip_packing_list_group_items_by_category', 'sv_wc_pip_packing_list_grouping', 10, 3 );
 
 ?>
